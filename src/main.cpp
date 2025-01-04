5bca608d61626bda6f7a1f798dde65eb085b7881
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2025-01-04 00:09:08 -0800

    proper event lööp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,22 @@
 #include <cstdlib>
-#include <iostream>
 
 #include <SDL2/SDL.h>
 
-#include "bases/obj.hpp"
-#include "bases/pos.hpp"
-#include "math/vec.hpp"
+#include "rnd/rend.hpp"
 
 int main() {
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "engine initalizing, commit " GIT_COMMIT_HASH);
     SDL_Init(SDL_INIT_EVERYTHING);
     atexit(SDL_Quit);
-    int i = 0;
-    while (i++ < 10) {
-        (new Object)->Reparent(gSceneRootNode);
+    Renderer r;
+    while (1) {
+        SDL_Event e; // temp until i setup proper event handling
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) goto exit_now;
+        }
+        SDL_FlushEvents(1, -1);
+        r.DoSceneDraws();
     }
-    Positionable* p = new Positionable; p->mName = "hai :3";
-    p->mPos = Vector3(7,1,5);
-    p->mRot = Vector3(6,5,0.2);
-    p->mScale = Vector3(6,85,5);
-    p->Reparent(gSceneRootNode);
-    gSceneRootNode->mChildObjs[6]->Reparent(gSceneRootNode->mChildObjs[4]);
-    Positionable* p2 = new Positionable;
-    p2->mPos = Vector3(1,6,8);
-    p2->mRot = Vector3(5,9,2);
-    p2->mScale = Vector3(1,0,0);
-    p2->Reparent(gSceneRootNode->FindByName("hai :3"));
-    Vector3 pos = p2->WorldPos();
-    Vector3 rot = p2->WorldRot();
-    Vector3 scl = p2->WorldScl();
-    std::cout << "pos test: " << pos.x << " " << pos.y << " " << pos.z << std::endl;
-    std::cout << "rot test: " << rot.x << " " << rot.y << " " << rot.z << std::endl;
-    std::cout << "scale test: " << scl.x << " " << scl.y << " " << scl.z << std::endl;
-    gSceneRootNode->Print(std::cout);
+exit_now:
     return 0;
 }
