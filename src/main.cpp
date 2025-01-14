dcaf5a093885da6921f1be0cdcc0ca9b7a8659f4
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2025-01-14 11:52:35 -0800

    more testing

--- src/main.cpp
+++ src/main.cpp
@@ -14,30 +14,31 @@ class Test : public Drawable, public Positionable {
     virtual ~Test() {}
     CLASS_NAME_CRUFT(Test)
     virtual void Draw() {
-        mRot.y += 3;
+        mRot.z += 3;
         glBegin(GL_TRIANGLE_STRIP);
 		glColor3f(1,0,0);
 		glVertex3f(0,0,0);
 		glColor3f(0,1,0);
-		glVertex3f(0.5,0.5,0.5);
+		glVertex3f(0.5,0,0);
 		glColor3f(0,0,1);
-		glVertex3f(-0.5,-0.5,0.5);
+		glVertex3f(0,0.5,0);
 		glColor3f(1,1,1);
-		glVertex3f(0.5,-0.5,0.5);
+		glVertex3f(0.5,0.5,0);
 		glEnd();
     }
 
-    void A() { mPos = Vector3(1, -2, -3); }
-    void B() { mPos = Vector3(1, 1, -2); mScale.x = 2; }
+    void A() { mPos = Vector3(0, -2, -3); }
+    void B() { mPos = Vector3(1, 1, -2); mRot.y = 90; }
 };
 
 int main() {
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "engine initializing, commit " GIT_COMMIT_HASH);
     SDL_Init(SDL_INIT_EVERYTHING);
     atexit(SDL_Quit);
-    Test a, b; a.A(); b.B();
+    Test a, b; a.A(); b.B(); Test c; c.mPos.z = -2;
     a.Reparent(gSceneRootNode);
     b.Reparent(gSceneRootNode);
+    c.Reparent(gSceneRootNode);
     Renderer r;
     while (1) {
         SDL_Event e; // temp until i setup proper event handling
