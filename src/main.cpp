0f1e6ddee3a39f5397b9bbc15f8421cc54ad18d6
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2025-03-04 13:01:00 -0800

    remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_log.h>
 #include <SDL2/SDL_surface.h>
 #include <cstdio>
@@ -7,8 +8,6 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_opengl.h>
 
-#include "bases/obj.hpp"
-#include "math/vec.hpp"
 #include "rnd/mesh.hpp"
 #include "rnd/rend.hpp"
 #include "rnd/tex.hpp"
@@ -36,42 +35,14 @@ int main(int argc, char** argv) {
 	Renderer r;
 
 	CallClassIniters();
-	
-	Mesh m;
-	m.Reparent(gSceneRootNode);
-
-	Texture* t = new Texture;
-	m.mTexture.reset(t);
-
-	SDL_Surface* srf = IMG_Load("bingus.png");
-	SDL_Surface* srf_fixed = SDL_ConvertSurfaceFormat(srf, SDL_PIXELFORMAT_RGB565, 0);
-	SDL_FreeSurface(srf);
-	t->LoadFromSurface(srf_fixed);
-	SDL_FreeSurface(srf_fixed);
-	
-
-	m.mVerts.reserve(4);
-	m.mVerts[0] = Vtx(0,0,0,0,0,-0.5,0,0.5);
-	m.mVerts[1] = Vtx(1,0,0,0,0,0.5,0,0.5);
-	m.mVerts[2] = Vtx(0,1,0,0,0,0,1,0);
-	m.mVerts[3] = Vtx(1,1,0,0,0,0,0,-0.5);
-	m.mFaces.resize(3);
-	m.mFaces[0] = Face({0,1,2});
-	m.mFaces[1] = Face({1,3,2});
-	m.mFaces[2] = Face({3,0,2});
-	m.mTransform.mPos = Vector3(0, 0, -3);
-	m.mTransform.mRot = Vector3(90, 0, 0);
-	m.InitDisplayList();
 
 	while (1) {
 		SDL_Event e; // temp until i setup proper event handling
 		while (SDL_PollEvent(&e)) {
 			if (e.type == SDL_QUIT) goto exit_now;
-			if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_w) Renderer::gWireframe = !Renderer::gWireframe;
 		}
 		SDL_FlushEvents(1, -1);
-		//m.mTransform.mRot.x += 1;
-		m.mTransform.mRot.y -= 3;
+
 		r.DoSceneDraws();
 
 		GLenum err = glGetError();
